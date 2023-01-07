29b27cd6c416058596d6f141915f19d7d592ef12
Brian Guzmán <b.guzman.m@me.com>
2023-01-07 13:56:08 -0300

    redirijo a backend deployado

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // require("dotenv").config();
-const url_api = "http://localhost:3001";
+const url_api = "http://udd-0701-production.up.railway.app";
 
 export const getUsers = async () => {
   // console.log(process.env.REACT_APP_API);
